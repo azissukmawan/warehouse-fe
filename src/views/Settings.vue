929eeefdcfc481d7a626ff2e9487a319c3a42d72
azissukmawan <sukmawanabdul@gmail.com>
2026-01-31 01:50:59 +0700

    feat: add Settings view with layout and initial content

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,1 +1,18 @@
- 
+
+<template>
+	<Layout pageTitle="Settings">
+		<main class="p-6">
+			<h1 class="text-2xl font-semibold">Settings</h1>
+			<p class="text-monday-gray mt-2">Pengaturan belum diisi. Tambahkan konten sesuai kebutuhan.</p>
+		</main>
+	</Layout>
+</template>
+
+<script>
+import Layout from '@/components/Layout.vue'
+
+export default {
+	name: 'Settings',
+	components: { Layout }
+}
+</script>
